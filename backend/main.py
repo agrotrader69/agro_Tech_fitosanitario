95c91d5156d3806c82508d6ccaf7990356a9ff62
agrotrader69 <agrotrader69@gmail.com>
2026-07-16 13:27:21 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -91,3 +91,6 @@ async def buscar_localidad(q: str):
                 break
 
     return resultados
+if __name__ == "__main__":
+        import uvicorn
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
