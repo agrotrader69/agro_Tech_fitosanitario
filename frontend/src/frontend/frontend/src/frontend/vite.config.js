f7f138e253feb98bce2535be0c116832abd37987
agrotrader69 <agrotrader69@gmail.com>
2026-07-16 10:16:50 +0200

    Delete frontend directory

--- frontend/src/frontend/frontend/src/frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Permite que las rutas funcionen perfectamente al desplegar en GitHub Pages
-})
